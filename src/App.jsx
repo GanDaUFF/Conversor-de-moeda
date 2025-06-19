672502cf3ae622bbf81a78c8976a24bf0c810e27
GandaUFF <qwert12121@hotmail.com>
2025-06-19 14:34:40 -0300

    feat: commit inicial e final do conversor de moedas

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CurrencyConverter from './components/currencyConverter'
+
+function App() {
+  
+
+  return (
+    <>
+      <CurrencyConverter/>
+    </>
+  )
+}
+
+export default App
